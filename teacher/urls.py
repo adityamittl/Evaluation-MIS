3b5b6976897320707f8f27be12e487b6cb9810d6
Advik Singhania <adviksinghania@gmail.com>
2022-11-15 23:29:58 +0530

    Edit profile page for teacher

--- teacher/urls.py
+++ teacher/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('teacher', teacher_func)
     # path('',administrator),
     # path('fetchstudent', fetchStudent),
     # path('instructor', manageInstructor),
