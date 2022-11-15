c68118ecf892d1f0c1d010dacf75b75af19e85d1
Aditya Mittal <aditmttl@gmail.com>
2022-11-15 23:34:19 +0530

    Create Course and edit Instructor

--- stem/views.py
+++ stem/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
+# from django.core import serializers
+
 
 import json
 from .excelRead import extractData, givePassword
 from .models import *
+
 
 presets = {
     "u": "UG",
@@ -45,7 +48,7 @@ def administrator(request):
 
     if request.method == "POST":
         studentData = request.FILES["studentDetails"]
-        fs = FileSystemStorage(location="media/")
+        fs = FileSystemStorage(location="media/enrollmentSheet")
         filename = fs.save(studentData.name, studentData)
         fetchedData = extractData("./media/" + filename)
         for rollnumber in fetchedData.keys():
@@ -86,6 +89,7 @@ def fetchStudent(request):
         }
         return JsonResponse(result)
 
+#Enroll new instructor into the institute.
 def manageInstructor(request):
     if request.method == 'POST':
         eid, fname,lname,department= request.POST.get('instructor'),request.POST.get('fname'), request.POST.get('lname'), request.POST.get('department')
@@ -98,9 +102,43 @@ def manageInstructor(request):
         
     return render(request, 'manageInstructor.html')
 
+# Check if the written employee id is is conflict with someone.
 def duplicateEID(request):
     data = request.body.decode('utf-8').split('=')[1]
     print(data)
     if teacherProfile.objects.filter(employeeId = data).count():
         return JsonResponse({'available' : False});
     return JsonResponse({'available' : True})
+
+#Create new course and view all course of the institute.
+def manageCourse(request):
+    courses = Subject.objects.all()
+    if request.method == 'POST':
+        sid = request.POST.get('sid').upper()
+        if Subject.objects.filter(subjectId = sid).count() >=1:
+            return render(request, 'course.html', context={'messages': 'warning','message':'Failed! Course ID already Exist','subjects' : courses})
+        sname = request.POST.get('sname')
+        ctype = request.POST.get('ctype')
+        credits = request.POST.get('credits')
+        sem = request.POST.get('offeredSem')
+        seats = request.POST.get('noseats')
+        Subject.objects.create(subjectId = sid,  subjectName = sname, credits = credits, subjectType = ctype, offeredSem = sem, totalSeats = seats).save()
+
+        return render(request, 'course.html', context={'messages': 'success','message':'Course has been introduced','subjects' : courses})
+
+    return render(request,'course.html',context= {'subjects' : courses})
+
+# To edit the corse using course ID. (aadhura kaam!!)
+def editCourse(request,cid):
+    sub = Subject.objects.get(subjectId = cid)
+    return render(request, 'cedit.html', context={'detail':sub})
+
+
+# for the registration access, that is allow admin to start registrations.
+
+def registrationSetup(request):
+    sessionYear.objects.create()
+    subjects = Subject.objects.all()
+    for course in subjects:
+        sessionSubject.objects.create(subject = course, sessionName = )
+    return render(request, 'registration.html')
